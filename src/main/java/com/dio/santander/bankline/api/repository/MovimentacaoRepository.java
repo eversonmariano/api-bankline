aaa4e03ccd83eae5aa384f66397ac82861cd5efe
Everson Mariano <mariano.computacao@gmail.com>
2022-05-09 19:40:48 -0300

    final

---
+++ src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
@@ -0,0 +1,13 @@
+package com.dio.santander.bankline.api.repository;
+
+import com.dio.santander.bankline.api.model.Movimentacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
+
+    public List<Movimentacao>findByIdConta(Integer idConta);
+
+
+}
